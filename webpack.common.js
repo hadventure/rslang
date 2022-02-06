fd6d1e90341de3e4ac00d8e17e83279bb826fb1a
hadventure <178652360+hadventure@users.noreply.github.com>
2022-02-07 00:16:47 +0300

    feat: add initial config

---
+++ webpack.common.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          // options: {
+          //   transpileOnly: true
+          // }
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          // MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                auto: true,
+                exportLocalsConvention: 'camelCase',
+                exportGlobals: true,
+                localIdentName: '[local]--[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader', // 3
+        ],
+      },
+      {
+        test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|mp3|json)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.scss'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      minify: false,
+      template: path.resolve(__dirname, './index.html'),
+      filename: 'index.html',
+      // favicon: './src/assets/car.ico',
+      inject: 'body',
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: 'src/assets', to: 'src/assets' },
+    //   ],
+    // }),
+  ],
+};
