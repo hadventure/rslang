548f09135a916ce4e80cf47f9182ff6e290556b4
hadventure <178652360+hadventure@users.noreply.github.com>
2022-02-08 00:54:27 +0300

    feat: add navigation structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,28 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Navigation from './components/navigation/navigation';
+import WordGroupList from './components/word-group-list/word-group-list';
 import WordList from './components/word-list/word-list';
 
 function App() {
   return (
-    <div>
-      <WordList />
-    </div>
+    <BrowserRouter basename="/">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<div>Main</div>} />
+        <Route path="tutorials">
+          <Route index element={<WordGroupList />} />
+          <Route path=":tutorial" element={<WordList />} />
+        </Route>
+
+        <Route path="/statistics" element={<div>Statistics</div>} />
+        <Route path="/auth" element={<div>Auth</div>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
